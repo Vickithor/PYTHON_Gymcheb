ec67ad04a331b12c66b4b786e9a638af2dc0c86a
Vickithor <karpeta.victor@gmail.com>
2026-03-25 18:26:07 +0100

    Create main.py

---
+++ Lekce_05_Vestavěné funkce/main.py
@@ -0,0 +1,36 @@
+# Představ si, že spravuješ e-shop s elektronikou.
+# Zde jsou ceny produktů v jednom nákupním košíku:
+
+ceny_kosiku = [1250.50, 499.00, 120.80, 5600.00, 890.00]
+# --------------------------------------------------------------------------
+
+# ÚLOHA 1: Základní přehled
+# Vypiš do konzole:
+# a) Kolik položek je v košíku (použij len)
+# b) Kolik stojí nejlevnější produkt (použij min)
+# c) Kolik stojí nejdražší produkt (použij max)
+
+print(f"Počet položek: {len(ceny_kosiku)}")
+# Sem doplň zbytek:
+
+
+# ÚLOHA 2: Celková tržba
+# Spočítej celkovou cenu celého košíku (použij sum)
+# Výsledek vypiš a zaokrouhli na celé koruny.
+
+# Sem doplň kód:
+
+
+# ÚLOHA 3: Průměrná cena
+# Vypočítej průměrnou cenu produktu v košíku.
+# Průměr = součet / počet
+# Výsledek vypiš zaokrouhlený na 2 desetinná místa pomocí f-stringu (:.2f)
+
+# Sem doplň kód:
+
+
+# BONUSOVÁ ÚLOHA: Rozdíl
+# Vypočítej, o kolik korun je nejdražší položka dražší než ta nejlevnější.
+# Výsledek vypiš.
+
+# Sem doplň kód:
