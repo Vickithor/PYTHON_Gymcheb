84396c4be6ca473c325ca59453a67c646c1bbe81
Vickithor <karpeta.victor@gmail.com>
2026-04-15 23:35:33 +0200

    Create main.py

---
+++ Lekce_07_Želví grafika a ASCII umění/main.py
@@ -0,0 +1,28 @@
+Úkoly k vypracování
+
+Úkol č. 1: Barevný čtverec (Turtle ⭐)
+
+Vytvoř program, který pomocí želvy vykreslí čtverec.
+
+    Každá strana bude mít jinou barvu (použij t.color()).
+
+    Strana čtverce bude dlouhá 100 kroků.
+
+
+Úkol č. 2: Dům na drakovi (Turtle ⭐⭐)
+
+Zkus nakreslit domeček (čtverec a na něm trojúhelník).
+
+    Střecha bude červená a bude vybarvená pomocí begin_fill() a end_fill().
+
+    Bonus: Pomocí t.penup() a t.pendown() přesuň želvu a nakresli vedle domu sluníčko.
+
+
+Úkol č. 3: ASCII Galerie (Text ⭐)
+Navštiv www.asciiart.eu a vyber si jedno zvíře nebo věc.
+
+    Ulož si tento obrázek do proměnné pomocí """ (```).
+
+    Program náhodně vylosuje číslo 1-2 (pomocí random.randint).
+
+    Pokud padne 1, vypiš obrázek. Pokud 2, napiš: "Dnes galerie zavřeno."
